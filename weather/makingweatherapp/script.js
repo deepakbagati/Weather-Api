97b74c6b8b19b0ad99028e8d4793029465545a06
Deepak Bagati <130148122+deepakbagati@users.noreply.github.com>
2023-11-14 15:55:58 +0530

    Add files via upload
    A weather API (Application Programming Interface) is a service that provides developers with access to real-time and historical weather data. This data includes information such as temperature, humidity, wind speed, atmospheric pressure, and more for a specific location and time. Weather APIs are typically used by developers to integrate weather information into their applications, websites, or services.
    By leveraging a weather API, developers can enhance their applications with dynamic and up-to-date weather information, enabling users to stay informed about current weather conditions and forecasts. These APIs often allow users to retrieve weather data for a specific location, obtain forecasts for future dates, and access information in various formats, such as JSON or XML.

---
+++ weather/makingweatherapp/script.js
@@ -0,0 +1,49 @@
+const apiKey="60716c87f0b9e8d5c57e94c685bc9204";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox=document.querySelector(".search input");
+const searchBtn=document.querySelector(".search button");
+const weatherIcon=document.querySelector(".weather-icon")
+
+async function checkWeather(city){
+    const response =await fetch(apiUrl +city + `&appid=${apiKey}`);
+
+    if(response.status==404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    }
+    else{
+        var data= await response.json();
+        
+        document.querySelector(".city").innerHTML =data.name;
+        document.querySelector(".temp").innerHTML =Math.round(data.main.temp)+ "°C";
+        document.querySelector(".humidity").innerHTML =data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML =data.wind.speed + " km/hr";
+    
+        if(data.weather[0].main=="Clouds"){
+            weatherIcon.src="clouds.png";
+        }
+        else if(data.weather[0].main=="Clear"){
+            weatherIcon.src="clear.png";
+        }
+        else if(data.weather[0].main=="Rain"){
+            weatherIcon.src="rain.png";
+        }
+        else if(data.weather[0].main=="Drizzle"){
+            weatherIcon.src="drizzle.png";
+        }
+        else if(data.weather[0].main=="Mist"){
+            weatherIcon.src="mist.png";
+        }
+        document.querySelector(".weather").style.display="block";
+        document.querySelector(".error").style.display="none";
+        }
+    }
+    function clearTextBox() {
+        let textBox = document.querySelector(".search input");
+        textBox.value = ''; // This line clears the text box
+      }
+      
+
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value); 
+})
